067b51a114c6fc1c599b2f363a7d12074937a3d3
ShinyQ_11 <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2020-03-17 16:24:34 +0700

    ADD | Setup Articles View, Setup Articles Dummy Data

--- markdown-blog/server.js
+++ markdown-blog/server.js
@@ -7,7 +7,26 @@ app.set('view engine','ejs')
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res)=> {
-    res.render('index')
+    const articles = [
+        {
+            title: 'Article 1',
+            createdAt: new Date(),
+            description: 'This Is Article 1'
+        },
+        {
+            title: 'Article 2',
+            createdAt: new Date(),
+            description: 'This Is Article 2'
+        },
+        {
+            title: 'Article 3',
+            createdAt: new Date(),
+            description: 'This Is Article 3'
+        },
+    ]
+
+    res.render('index', {articles: articles})
 })
 
 app.listen(5000)
+
